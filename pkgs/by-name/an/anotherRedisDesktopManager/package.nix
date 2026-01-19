7f507ccf899506e2e0d6c77b1c279330b9d2f4a0
luozenan <162779246+luozenan@users.noreply.github.com>
2026-01-19 16:32:34 +0800

    Update package.nix

--- pkgs/by-name/an/anotherRedisDesktopManager/package.nix
+++ pkgs/by-name/an/anotherRedisDesktopManager/package.nix
@@ -8,8 +8,7 @@ let
 src = fetchurl {
   url = "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v1.7.1/Another-Redis-Desktop-Manager-linux-1.7.1-x86_64.AppImage";
   # 替换为真实的sha256 hash（带sha256-前缀）
-  hash = "sha256-0m64isixgv6yx7h69x81nq97lx732dvvcdj1c7l9llp1qs7bir2y";
-";
+  hash = "sha256-XuS4jsbhUproYUE2tncT43R6ErYB9WTg6d7s16OOxFQ=";
 };
 
   appimageContents = pkgs.appimageTools.extract {
@@ -32,14 +31,14 @@ pkgs.appimageTools.wrapAppImage {
   extraInstallCommands = ''
     # 复制桌面文件（和微信的cp wechat.desktop逻辑一致）
     mkdir -p $out/share/applications
-    cp ${appimageContents}/anotherRedisDesktopManager.desktop $out/share/applications/
+    cp ${appimageContents}/another-redis-desktop-manager.desktop $out/share/applications/
 
     # 复制图标（和微信的cp wechat.png逻辑一致，仅尺寸不同）
     mkdir -p $out/share/icons/hicolor/256x256/apps
-    cp ${appimageContents}/usr/share/icons/hicolor/256x256/apps/anotherRedisDesktopManager.png $out/share/icons/hicolor/256x256/apps/
+    cp ${appimageContents}/another-redis-desktop-manager.png $out/share/icons/hicolor/256x256/apps/
 
     # 替换Exec字段（和微信的--replace-fail AppRun wechat逻辑一致）
-    substituteInPlace $out/share/applications/anotherRedisDesktopManager.desktop --replace-fail AppRun anotherRedisDesktopManager
+    substituteInPlace $out/share/applications/another-redis-desktop-manager.desktop --replace-fail AppRun another-redis-desktop-manager
   '';
 
 }
