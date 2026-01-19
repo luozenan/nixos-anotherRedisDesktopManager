0cdeb22b6b9f1c82a0afd3ad4608e262d2bf770d
luozenan <162779246+luozenan@users.noreply.github.com>
2026-01-19 15:17:45 +0800

    Update package.nix

--- pkgs/by-name/an/anotherRedisDesktopManager/package.nix
+++ pkgs/by-name/an/anotherRedisDesktopManager/package.nix
@@ -7,7 +7,7 @@ let
 
   src = fetchurl {
           url = "https://github.com/qishibo/AnotherRedisDesktopManager/releases/download/v1.7.1/Another-Redis-Desktop-Manager-linux-1.7.1-x86_64.AppImage";
-          hash = "sha256-0jvx3pvjw90mcqgh8y0mmm1c1pd25g8s03qyxv92dlaxgrg32xal";
+          hash = lib.fakeSha256;
         };
 
   appimageContents = pkgs.appimageTools.extract {
